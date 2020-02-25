6151d2cbd46f7082e87865d51f646c1a296d0474
SIRISHAA <ayyagari.siri@gmail.com>
2020-02-25 11:40:46 +0100

    bicycle store

---
+++ bicycle_store.sql
@@ -0,0 +1,7 @@
+#Design a store database
+# select statements to order your items based on price and at least one statistic about the show items
+
+SELECT *
+FROM own_store
+WHERE bike_price>350
+ORDER BY bike_price;
