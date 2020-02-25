445560197171b2f50066eb950b4b46743e033097
SIRISHAA <ayyagari.siri@gmail.com>
2020-02-25 15:26:26 +0100

    Foreign keys

---
+++ console.sql
@@ -0,0 +1,14 @@
+INSERT INTO customers(customer_id, first_name, last_name, birth_date, phone, address, city, state, points)
+VALUES (DEFAULT,'john','smith','1945-08-21','NULL','address','city','CA',DEFAULT);
+
+INSERT INTO orders(customer_id, order_date, comments, shipped_date, shipper_id)
+VALUES(LAST_INSERT_ID(),'1945-08-21',NULL,'1945-08-21',2);
+
+USE sql_invoicing;
+DELETE
+FROM invoices
+WHERE invoice_id =10;
+
+SELECT *
+FROM invoices;
+
